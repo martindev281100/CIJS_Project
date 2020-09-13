0f1ee84ce727bdef1a9f9df1c71945e4dddfb034
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-09-13 13:13:07 +0700

    update commit

--- js/model.js
+++ js/model.js
@@ -123,6 +123,9 @@ model.logInWithFacebook = () => {
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
+    if (result.emailVerified == false) {
+      firebase.auth().currentUser.sendEmailVerification();
+    }
     console.log(result)
     // ...
   }).catch(function (error) {
