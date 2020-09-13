30886f4dd6e59013095d44af1562ec10a82ce81b
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-09-13 21:09:21 +0700

    update commit

--- js (1)/index.js
+++ js (1)/index.js
@@ -12,7 +12,6 @@ window.onload = () => {
   firebase.initializeApp(firebaseConfig);
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      console.log(user)
       var uid = user.uid
       model.presence();
       model.currentUser = {
