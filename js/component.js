69c4e8aed11b1c1358745dc45cf52a8af4bfe2f1
Manhlt99 <luuthemanhgch17279@gmail.com>
2020-09-13 22:21:58 +0700

    update 10:21pm

--- js/component.js
+++ js/component.js
@@ -59,42 +59,36 @@ component.registerPage = `
 component.loginPage = `
   <div class="login-container">
     <form id="login-form">
-    <div class="login-header">LOGIN</div>
-    <div class="input-wrapper form-group">
-      <input
-        type="email"
-        class="form-control"
-        placeholder="Email"
-        name="email"
-      />
-      <div class="error" id="email-error"></div>
-    </div>
-    <div class="input-wrapper form-group">
-      <input
-        type="password"
-        class="form-control"
-        placeholder="Password"
-        name="password"
-      />
-      <div class="error" id="password-error"></div>
-    </div>
-
-    <div class="form-action input-wrapper">
-      <button
-        class="btn btn-primary btn-login cursor-pointer"
-        type="submit"
-      >
-        Login
-      </button>
-            <button id="btn_google"> Google </button>
-            <button id="btn_facebook"> Facebook </button>
-
-      <div class="redirect">
-          Don't have an account?
-        <span id="redirect-to-register" class="cursor-pointer">Register</span>
+      <div class="login-header">LOGIN</div>
+      <div class="input-wrapper form-group">
+        <input type="email" class="form-control" placeholder="Email" name="email" />
+        <div class="error" id="email-error"></div>
       </div>
-    </div>
-  </form>   
+      <div class="input-wrapper form-group">
+        <input type="password" class="form-control" placeholder="Password" name="password" />
+        <div class="error" id="password-error"></div>
+      </div>
+
+      <div class="form-action input-wrapper">
+        <button class="btn btn-primary btn-login cursor-pointer" type="submit">
+          Login
+        </button>
+
+        <div class="login-by-social-account">
+          <div class="text">
+            <span>
+              Or login with
+            </span>
+          </div>
+          <button id="btn_google"><i class="fab fa-google"></i> Google </button>
+          <button id="btn_facebook"><i class="fab fa-facebook-square"></i> Facebook </button>
+        </div>
+        <div class="redirect">
+          Don't have an account?
+          <span id="redirect-to-register" class="cursor-pointer">Register</span>
+        </div>
+      </div>
+    </form>
   </div>`;
 
 component.homePage = `
