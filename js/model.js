1e86ffdaf96597ad3169fd83b719e23e60645ad2
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-09-19 13:46:30 +0700

    update commit

--- js/model.js
+++ js/model.js
@@ -137,9 +137,9 @@ model.logInWithGoogle = () => {
     // ...
   })
 }
-model.logInWithFacebook = async () => {
+model.logInWithFacebook = () => {
   var provider = new firebase.auth.FacebookAuthProvider();
-  await firebase.auth().signInWithPopup(provider).then(function (result) {
+  firebase.auth().signInWithPopup(provider).then(async function (result) {
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       var token = result.credential.accessToken;
       // The signed-in user info.
